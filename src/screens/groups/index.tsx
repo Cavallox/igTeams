bdc08959ae62e51e311f6731d2c399c46feed950
Cavallox <felipe_cavallo@hotmail.com>
2024-01-28 18:24:15 -0300

    add assets

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export function Groups() {
   return (
     <View style={styles.container}>
-      <Text>Groups</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }
