4a1b2d130e4804a9db17046651d119baaeecba3e
Cavallox <felipe_cavallo@hotmail.com>
2024-01-29 15:51:56 -0300

    estilizando StatusBar

--- App.tsx
+++ App.tsx
@@ -4,12 +4,18 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { Groups } from '@screens/groups';
 import theme from './src/theme';
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
   return(
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   
