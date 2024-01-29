c616f815605ea5b5567ed6163111c1a328333e23
Cavallox <felipe_cavallo@hotmail.com>
2024-01-29 16:57:15 -0300

    Criando componente Highlight

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-//import { Container, Title } from './styles';
+//import { Container } from './styles';
 import * as S from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+
+      <Highlight title="Turmas" subtitle="Jogue com sua turma!" />
     </S.Container>
   );
 }
